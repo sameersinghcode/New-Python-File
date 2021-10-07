0ec014b586fcb8db29c9db1387355b9ab19418f4
Sameer Singh <sameergr20@gmail.com>
2021-10-07 10:28:13 +0530

    Add files via upload

---
+++ guldaudi selection new try.py
@@ -0,0 +1,94 @@
+sum1=0
+while True:
+    a=int(input('input the variety number:  '))
+    if a == 1:
+        sum1=sum1+20
+    elif a == 2:
+        sum1=sum1+20
+    elif a == 3:
+        sum1=sum1+20
+    elif a == 4:
+        sum1=sum1+20
+    elif a == 5:
+        sum1=sum1+20
+    elif a == 6:
+        sum1=sum1+20
+    elif a == 7:
+        sum1=sum1+20
+    elif a == 8:
+        sum1=sum1+20
+    elif a == 9:
+        sum1=sum1+20
+    elif a == 10:
+        sum1=sum1+20
+    elif a == 11:
+        sum1=sum1+20
+    elif a == 12:
+        sum1=sum1+20
+    elif a == 13:
+        sum1=sum1+50
+    elif a == 14:
+        pass
+    elif a == 15:
+        sum1=sum1+20
+    elif a == 16:
+        pass
+    elif a == 17:
+        sum1=sum1+20
+    elif a == 18:
+        sum1=sum1+20
+    elif a == 19:
+        sum1=sum1+20
+    elif a == 20:
+        sum1=sum1+20
+    elif a == 21:
+        sum1=sum1+20
+    elif a == 22:
+        sum1=sum1+20
+    elif a == 23:
+        sum1=sum1+20
+    elif a == 24:
+        sum1=sum1+50
+    elif a == 25:
+        sum1=sum1+20
+    elif a == 26:
+        sum1=sum1+20
+    elif a == 27:
+        sum1=sum1+20
+    elif a == 28:
+        sum1=sum1+20
+    elif a == 29:
+        sum1=sum1+20
+    elif a == 30:
+        sum1=sum1+20
+    elif a == 31:
+        sum1=sum1+20
+    elif a == 32:
+        sum1=sum1+20
+    elif a == 33:
+        pass
+    elif a == 34:
+        pass
+    elif a == 35:
+        pass
+    elif a == 36:
+        sum1=sum1+50
+    elif a == 37:
+        sum1=sum1+20
+    elif a == 38:
+        sum1=sum1+50
+    elif a == 39:
+        sum1=sum1+20
+    elif a == 40:
+        sum1=sum1+20
+    elif a == 41:
+        sum1=sum1+20
+    elif a == 42:
+        sum1=sum1+20
+    elif a == 43:
+        sum1=sum1+20
+    elif a == 44:
+        sum1=sum1+50
+    else:
+        break
+print('Sum of the Varities: ',sum1)
